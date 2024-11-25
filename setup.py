57923f2c2be7b9327fb283c3ddef2dcf525ed3d9
Giã Dương Đức Minh <giaduongducminh@gmail.com>
2024-11-25 12:30:36 +0700

    Change PyPI package name

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="zalosdk",
+    name="zalopysdk",
     version=os.getenv("VERSION", "0.1.0"),
     author="Giã Dương Đức Minh",
     author_email="giaduongducminh@gmail.com",
